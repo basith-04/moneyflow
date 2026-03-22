f600841d0c7ba853e4a57ff7ec2aa2fe3a7fc5d5
Abdul basith <basithofficialmail01@gmail.com>
2026-03-22 10:48:54 +0530

    Create basic server setup in src/index.js with expense and category routes

---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+import express from 'express'
+import {expenseRouter}  from './routes/expenseRouter.js'
+import { categoryRouter } from './routes/categoryRouter.js'
+const app =express()
+const PORT=8000
+app.use(express.json())
+app.use('/expenses',expenseRouter)
+app.use('/category',categoryRouter)
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`)
+}).on('error', (err) => {
+  console.error('Failed to start server:', err)
+}) 
