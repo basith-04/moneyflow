7df45af3c5075a7298d873d83373e1f5320c5b1b
Abdul basith <basithofficialmail01@gmail.com>
2026-03-22 13:29:16 +0530

    Add CORS middleware and update package dependencies

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,13 @@
 import dotenv from 'dotenv'
 dotenv.config()
-
+import cors from 'cors';
 import express from 'express'
 import {expenseRouter}  from './routes/expenseRouter.js'
 import { categoryRouter } from './routes/categoryRouter.js'
 const app =express()
 const PORT=8000
+
+app.use(cors());
 app.use(express.json())
 app.use('/expenses',expenseRouter)
 app.use('/category',categoryRouter)
