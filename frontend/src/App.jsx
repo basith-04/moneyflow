fcdbe407ad913b92f20f1b6c836965479de8cd85
Abdul basith <basithofficialmail01@gmail.com>
2026-03-27 17:00:02 +0530

    Add React Router for navigation and update package dependencies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,18 @@
 import { useState } from 'react'
+import { Routes , Route,BrowserRouter } from "react-router-dom";
+import Expenses from './pages/Expenses';
+import Categories from './pages/Categories';
 function App() {
-  return <h1>MoneyFlow</h1>;
+  return  <BrowserRouter> <div> 
+    
+    <Routes>
+      <Route path='/' element ={<Expenses />}/>
+      <Route path='/categories' element ={<Categories />}/>
+      
+    </Routes>
+
+  </div>
+  </BrowserRouter>
 }
 
 export default App
