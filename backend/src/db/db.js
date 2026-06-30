6e531980295836418530f4e77422a526b6830772
basith <basithofficialmail01@gmail.com>
2026-07-01 01:04:40 +0530

    Refactor: migrate db connection from mysql2 to pg

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,12 +1,11 @@
-import mysql from 'mysql2/promise'
+import pg from 'pg'
+export const pool =new pg.Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+    // ,ssl: { rejectUnauthorized: false }
+});
 
-const pool =mysql.createPool({
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    database:process.env.DB_NAME,
-    port:process.env.DB_PORT,
-    waitForConnections:true,
-    connectionLimit:10
-})
 export default pool;
